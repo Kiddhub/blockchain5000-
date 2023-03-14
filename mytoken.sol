536e301df1c328891b30f14aac4a5ed017a08c89
OdiK <quangminh2001.hust@gmail.com>
2023-03-14 19:21:01 +0700

    second

--- mytoken.sol
+++ mytoken.sol
@@ -8,6 +8,7 @@ contract MyToken {
     uint256 public totalSupply = 1000000;
 
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value );
     mapping (address => mapping(address => uint256)) allowed;
     mapping (address => uint256) balances;
     constructor() {
@@ -31,6 +32,16 @@ contract MyToken {
         emit Transfer(_from,_to,_value);
         return true;
 
+    }
+
+    function approve (address _spender, uint256 _value) public returns (bool success){
+        allowed[msg.sender][_spender] = _value;
+        emit Approval(msg.sender,_spender,_value);
+        return true;
+    }
+    function allowance (address _owner, address _spender) public view returns (uint256 remaining){
+        remaining = allowed[_owner][_spender];
+        return remaining;
     }
 
 }
